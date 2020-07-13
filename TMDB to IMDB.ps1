437e5e3220252245633324c5267e7eff0a8278a9
timpan8 <32546789+timpan8@users.noreply.github.com>
2020-07-13 17:39:41 +0200

    Add files via upload

---
+++ TMDB to IMDB.ps1
@@ -0,0 +1,18 @@
+﻿#TMDB APIKEY https://www.themoviedb.org/settings/api
+$APIKEY = "8d6d91941230817f7807d643736e8a49"
+
+#List with TMDB ID's 
+$LISTOMOVIES = Get-Content C:\temp\lista.txt
+
+ForEach($movie_id in $LISTOMOVIES){
+
+
+
+$IMDBURI = "https://api.themoviedb.org/3/movie/" + $movie_id + "/external_ids?api_key=" + $APIKEY 
+$MOVIE = Invoke-WebRequest -Uri $IMDBURI
+
+$IMDBMOVIERESULTS = (ConvertFrom-Json $MOVIE.Content)
+
+
+Write-Host  $IMDBMOVIERESULTS.imdb_id $IMDBMOVIERESULTS.id
+}
